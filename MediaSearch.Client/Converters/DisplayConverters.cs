b6eac4b9bbe5ec7c629b52dee58207f92e03952a
Luc Bolly <lbolly@hotmail.com>
2022-02-03 16:40:27 +0100

    Fixed DisplayConverters

---
+++ MediaSearch.Client/Converters/DisplayConverters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MediaSearch.Client;
+
+public static class DisplayConverters {
+
+  public static string DisplaySizeInUnit(long size) {
+    const long KB = 1024;
+    const long MB = 1024 * KB;
+    const long GB = 1024 * MB;
+
+    if (size > GB) {
+      double Size = (double)size / (double)GB;
+      return $"{Size:N3} GB";
+
+    }
+    if (size > MB) {
+      double Size = (double)size / (double)MB;
+      return $"{Size:N0} MB";
+    }
+
+    if (size > KB) {
+      double Size = (double)size / (double)KB;
+      return $"{Size:N0} KB";
+    }
+
+    return $"{size} B";
+  }
+
+  public static string DisplayPathNormalized(string path) {
+
+    string[] Components = path.Split(new char[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+    return string.Join(" > ", Components);
+  }
+}
